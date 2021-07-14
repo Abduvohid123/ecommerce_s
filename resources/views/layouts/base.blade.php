e42f82cccf6d63f714c6688ea5c17c3567287bef
Abduvohid123 <abduvohid.mirzamahmudov@gmal.com>
2021-07-14 11:10:36 +0500

    shop-component ga iwlov berildi

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,9 @@
     <link
         href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext"
         rel="stylesheet">
+
     <link
+
         href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext"
         rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.css')}}">
